678b48631c3cd1e117217e512296657467d3f985
toni <thienchuong004@gmail.com>
2024-12-28 00:33:59 +0700

    update

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -1,8 +1,6 @@
 package routers
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	c "github.com/thienchuong/golang-ecommerce-backend-api/internal/controller"
 )
@@ -15,13 +13,4 @@ func NewRouter() *gin.Engine {
 		v1.PUT("/user/1", c.NewUserController().GetUserByID)
 	}
 	return r
-}
-
-func pong(c *gin.Context) {
-	name := c.Param("name")
-	udi := c.Query("udi")
-	c.JSON(http.StatusOK, gin.H{
-		"message": "pong" + name,
-		"udi":     udi,
-	})
 }
